1d9bb5769b6659ee9336a3d5aa8f6518cd697849
MJ-Dev92 <minjae920802@naver.com>
2024-09-19 01:04:17 +0900

    Add : MyCart 비어있을 때 컴포넌트 추가

--- src/components/myPage/myCart/MyCart.tsx
+++ src/components/myPage/myCart/MyCart.tsx
@@ -6,6 +6,7 @@ import CartItem from "./CartItem";
 import PriceCard from "./PriceCard";
 import useCart from "../../../hook/useCart";
 import Button from "../../ui/Button";
+import NoProductsFound from "../../main/NoProductsFound";
 
 import TotalPlusIcon from "../../../assets/icons/totalPlus.svg";
 import EqualsIcon from "../../../assets/icons/totalEquals.svg";
@@ -36,30 +37,36 @@ export default function MyCart() {
   }
   return (
     <div className="container w-[600px]">
-      <div className="flex text-lg gap-1 text-left px-11 mb-[10px]">
-        <p className="font-bold mr-[5px]">전체</p>
-        <p className="text-lightgray">{carts?.length}</p>
-      </div>
-      <p className="border border-[#D9D9D9] w-[520px] m-auto mt-[20px] mb-[30px]"></p>
-      <ul className="px-11 py-2 pb-4">
-        {carts &&
-          carts.map((product) => (
-            <CartItem key={product.id} carts={product} userId={userId} />
-          ))}
-      </ul>
-      <p className="border border-[#D9D9D9] w-[520px] m-auto mt-[30px] mb-[45px]"></p>
-      <div className="flex justify-between items-center mb-10 px-2 md:px-8">
-        <PriceCard text="상품 총액" price={totalPrice} />
-        <img src={TotalPlusIcon} alt="TotalPlusIcon" />
-        <PriceCard text="배송액" price={SHIPPING} />
-        <img src={EqualsIcon} alt="EqualsIcon" />
-        <PriceCard text="총가격" price={totalPrice + SHIPPING} />
-      </div>
-      <Button
-        text="주문하기"
-        className="w-[520px] mb-[100px] mt-[10px] bg-puple text-white border border-puple hover:bg-white hover:text-puple"
-        onClick={handleClickPayment}
-      />
+      {carts?.length !== 0 ? (
+        <div>
+          <div className="flex text-lg gap-1 text-left px-11 mb-[10px]">
+            <p className="font-bold mr-[5px]">전체</p>
+            <p className="text-lightgray">{carts?.length}</p>
+          </div>
+          <p className="border border-[#D9D9D9] w-[520px] m-auto mt-[20px] mb-[30px]"></p>
+          <ul className="px-11 py-2 pb-4">
+            {carts &&
+              carts.map((product) => (
+                <CartItem key={product.id} carts={product} userId={userId} />
+              ))}
+          </ul>
+          <p className="border border-[#D9D9D9] w-[520px] m-auto mt-[30px] mb-[45px]"></p>
+          <div className="flex justify-between items-center mb-10 px-2 md:px-8">
+            <PriceCard text="상품 총액" price={totalPrice} />
+            <img src={TotalPlusIcon} alt="TotalPlusIcon" />
+            <PriceCard text="배송액" price={SHIPPING} />
+            <img src={EqualsIcon} alt="EqualsIcon" />
+            <PriceCard text="총가격" price={totalPrice + SHIPPING} />
+          </div>
+          <Button
+            text="주문하기"
+            className="w-[520px] mb-[100px] mt-[10px] bg-puple text-white border border-puple hover:bg-white hover:text-puple"
+            onClick={handleClickPayment}
+          />
+        </div>
+      ) : (
+        <NoProductsFound text="장바구니가 비어있어요." />
+      )}
     </div>
   );
 }
