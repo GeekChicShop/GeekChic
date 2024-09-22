c8dd35a5fc60c97314a456e286cf3ffbbe1493b8
MJ-Dev92 <minjae920802@naver.com>
2024-09-22 21:28:05 +0900

    Add : orderDetailSkeleton  레이아웃 수정

--- src/components/skeleton/OrdersDetailSkeleton.tsx
+++ src/components/skeleton/OrdersDetailSkeleton.tsx
@@ -23,30 +23,30 @@ export default function OrdersDetailSkeleton() {
         <div className="w-[70px] h-[20px] bg-gray-300 rounded"></div>
       </div>
       <div className="flex justify-end mr-[40px] mt-[10px]">
-        <div className="w-[70px] h-[25px] bg-gray-300 rounded"></div>
+        <div className="w-[70px] h-[20px] bg-gray-300 rounded"></div>
       </div>
       <p className="border border-[#D9D9D9] w-[520px] m-auto mt-[40px]"></p>
 
-      <div className="text-left ml-[40px] mt-[40px]">
-        <div className="w-[70px] h-[25px] bg-gray-300 rounded"></div>
+      <div className="ml-[40px] mt-[40px]">
+        <div className="w-[100px] h-[25px] bg-gray-300 rounded"></div>
       </div>
       <div className="mt-[30px]">
-        <div className="w-[70px] h-[25px] bg-gray-300 rounded mr-[470px] mb-[10px]"></div>
-        <div className="w-[70px] h-[25px] bg-gray-300 rounded ml-[50px]"></div>
+        <div className="w-[50px] h-[20px] bg-gray-300 rounded ml-[50px] mb-[10px]"></div>
+        <div className="w-[150px] h-[20px] bg-gray-300 rounded ml-[50px] mb-[10px]"></div>
         <p className="border border-[#D9D9D9] w-[520px] m-auto"></p>
-        <div className="w-[70px] h-[25px] bg-gray-300 rounded mr-[450px] mt-[10px] mb-[10px]"></div>
-        <div className="w-[70px] h-[25px] bg-gray-300 rounded ml-[50px]"></div>
+        <div className="w-[70px] h-[20px] bg-gray-300 rounded ml-[50px] mt-[10px] mb-[10px]"></div>
+        <div className="w-[150px] h-[20px] bg-gray-300 rounded ml-[50px] mb-[10px]"></div>
         <p className="border border-[#D9D9D9] w-[520px] m-auto"></p>
-        <div className="w-[70px] h-[25px] bg-gray-300 rounded mr-[460px] mt-[10px] mb-[10px]"></div>
-        <div className="w-[70px] h-[25px] bg-gray-300 rounded ml-[50px]"></div>
-        <p className="border border-[#D9D9D9] w-[520px] m-auto "></p>
+        <div className="w-[70px] h-[20px] bg-gray-300 rounded ml-[50px] mt-[10px] mb-[10px]"></div>
+        <div className="w-[150px] h-[20px] bg-gray-300 rounded ml-[50px] mb-[10px]"></div>
+        <p className="border border-[#D9D9D9] w-[520px] m-auto"></p>
       </div>
 
-      <div className="text-left ml-[40px] mt-[40px] ">
-        <div className="w-[70px] h-[25px] bg-gray-300 rounded mb-[5px]"></div>
+      <div className="ml-[50px] mt-[40px] ">
+        <div className="w-[100px] h-[25px] bg-gray-300 rounded mb-[5px]"></div>
       </div>
       <div className="mt-[20px]">
-        <div className="w-[70px] h-[25px] bg-gray-300 rounded ml-[50px]"></div>
+        <div className="w-[100px] h-[20px] bg-gray-300 rounded ml-[50px] mb-[10px]"></div>
         <p className="border border-[#D9D9D9] w-[520px] m-auto mb-[50px]"></p>
       </div>
     </div>
